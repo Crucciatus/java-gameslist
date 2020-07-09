6a1ca1a32644083a5b9d9453da6ffbb937aac887
Crucciatus <juliocesarafs2@gmail.com>
2020-07-09 18:44:50 -0300

    refactoring: rename the testGameModel to TestGameModel

--- src/test/TestGameModel.java
+++ src/test/TestGameModel.java
@@ -2,9 +2,10 @@ package test;
 
 import model.Game;
 
-public class testGameModel {
+public class TestGameModel {
   public static void main(String[] args) {
     Game game = new Game();
     System.out.println("Game: "+game);
+    Game game02 = new Game();
   }
 }
