aab45b4b8c8897a2a3a159332d6742f569c37e3a
Crucciatus <juliocesarafs2@gmail.com>
2020-07-09 19:17:17 -0300

    defines the methods of the GamePersistence class

--- src/persistence/GamePersistence.java
+++ src/persistence/GamePersistence.java
@@ -1,4 +1,49 @@
 package persistence;
 
+import model.Game;
+
+import java.util.List;
+
 public class GamePersistence {
+
+  /**
+   * adds the game to be saved to the list
+   * @param game
+   */
+  public void add(Game game) {
+    //TODO do the implementation
+  }
+
+  /**
+   * remove the reported game from the game list
+   * @param game - game to be removed
+   * @return - inform if the game as been removed from the game list
+   */
+  public boolean remove(Game game) {
+    //TODO do the implementation
+    return false;
+  }
+
+  /**
+   * reads the file from disk
+   */
+  public void readArchive() {
+
+  }
+
+  /**
+   * record the games list on disk
+   * overwriting the reported file if it exists
+   */
+  public void recordArchive() {
+
+  }
+
+  /**
+   * get list of games read
+   * @return
+   */
+  public List<Game> getGames() {
+    return null;
+  }
 }
